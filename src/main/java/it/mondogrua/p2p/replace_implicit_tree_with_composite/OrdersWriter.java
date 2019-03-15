2d0ed2081efe512dde937b1ee2c17a49ae8ca27c
rbr0k3r <raul.araya@brokergenius.com>
2019-03-15 11:28:16 +0100

    Extract method writeProductsTo

--- src/main/java/it/mondogrua/p2p/replace_implicit_tree_with_composite/OrdersWriter.java
+++ src/main/java/it/mondogrua/p2p/replace_implicit_tree_with_composite/OrdersWriter.java
@@ -5,11 +5,11 @@ public class OrdersWriter {
     private Orders orders;
 
     public OrdersWriter(Orders orders) {
-      this.orders = orders;
+        this.orders = orders;
     }
 
     public String getContents() {
-      StringBuffer xml = new StringBuffer();
+        StringBuffer xml = new StringBuffer();
         writeOrderTo(xml);
         return xml.toString();
     }
@@ -17,38 +17,42 @@ public class OrdersWriter {
     private void writeOrderTo(StringBuffer xml) {
         xml.append("<orders>");
         for (int i = 0; i < orders.getOrderCount(); i++) {
-          Order order = orders.getOrder(i);
-          xml.append("<order");
-          xml.append(" id='");
-          xml.append(order.getOrderId());
-          xml.append("'>");
-          for (int j=0; j < order.getProductCount(); j++) {
-              Product product = order.getProduct(j);
-              xml.append("<product");
-              xml.append(" id='");
-              xml.append(product.getID());
-              xml.append("'");
-              xml.append(" color='");
-              xml.append(colorFor(product));
-              xml.append("'");
-              if (product.getSize() != ProductSize.NOT_APPLICABLE) {
+            Order order = orders.getOrder(i);
+            xml.append("<order");
+            xml.append(" id='");
+            xml.append(order.getOrderId());
+            xml.append("'>");
+            writeProductsTo(xml, order);
+            xml.append("</order>");
+        }
+        xml.append("</orders>");
+    }
+
+    private void writeProductsTo(StringBuffer xml, Order order) {
+        for (int j = 0; j < order.getProductCount(); j++) {
+            Product product = order.getProduct(j);
+            xml.append("<product");
+            xml.append(" id='");
+            xml.append(product.getID());
+            xml.append("'");
+            xml.append(" color='");
+            xml.append(colorFor(product));
+            xml.append("'");
+            if (product.getSize() != ProductSize.NOT_APPLICABLE) {
                 xml.append(" size='");
                 xml.append(sizeFor(product));
                 xml.append("'");
-              }
-              xml.append(">");
-              xml.append("<price");
-              xml.append(" currency='");
-              xml.append(currencyFor(product));
-              xml.append("'>");
-              xml.append(product.getPrice());
-              xml.append("</price>");
-              xml.append(product.getName());
-              xml.append("</product>");
             }
-          xml.append("</order>");
+            xml.append(">");
+            xml.append("<price");
+            xml.append(" currency='");
+            xml.append(currencyFor(product));
+            xml.append("'>");
+            xml.append(product.getPrice());
+            xml.append("</price>");
+            xml.append(product.getName());
+            xml.append("</product>");
         }
-        xml.append("</orders>");
     }
 
 
@@ -64,4 +68,4 @@ public class OrdersWriter {
         return product.getColor();
     }
 
-  }
+}
