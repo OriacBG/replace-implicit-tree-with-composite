1408ba4914cb1cbea37853522d53d1b6d94c734f
rbr0k3r <raul.araya@brokergenius.com>
2019-03-15 11:26:31 +0100

    Extract to method writerOrderTo

--- src/main/java/it/mondogrua/p2p/replace_implicit_tree_with_composite/OrdersWriter.java
+++ src/main/java/it/mondogrua/p2p/replace_implicit_tree_with_composite/OrdersWriter.java
@@ -10,41 +10,45 @@ public class OrdersWriter {
 
     public String getContents() {
       StringBuffer xml = new StringBuffer();
-      xml.append("<orders>");
-      for (int i = 0; i < orders.getOrderCount(); i++) {
-        Order order = orders.getOrder(i);
-        xml.append("<order");
-        xml.append(" id='");
-        xml.append(order.getOrderId());
-        xml.append("'>");
-        for (int j=0; j < order.getProductCount(); j++) {
-            Product product = order.getProduct(j);
-            xml.append("<product");
-            xml.append(" id='");
-            xml.append(product.getID());
-            xml.append("'");
-            xml.append(" color='");
-            xml.append(colorFor(product));
-            xml.append("'");
-            if (product.getSize() != ProductSize.NOT_APPLICABLE) {
-              xml.append(" size='");
-              xml.append(sizeFor(product));
+        writeOrderTo(xml);
+        return xml.toString();
+    }
+
+    private void writeOrderTo(StringBuffer xml) {
+        xml.append("<orders>");
+        for (int i = 0; i < orders.getOrderCount(); i++) {
+          Order order = orders.getOrder(i);
+          xml.append("<order");
+          xml.append(" id='");
+          xml.append(order.getOrderId());
+          xml.append("'>");
+          for (int j=0; j < order.getProductCount(); j++) {
+              Product product = order.getProduct(j);
+              xml.append("<product");
+              xml.append(" id='");
+              xml.append(product.getID());
               xml.append("'");
+              xml.append(" color='");
+              xml.append(colorFor(product));
+              xml.append("'");
+              if (product.getSize() != ProductSize.NOT_APPLICABLE) {
+                xml.append(" size='");
+                xml.append(sizeFor(product));
+                xml.append("'");
+              }
+              xml.append(">");
+              xml.append("<price");
+              xml.append(" currency='");
+              xml.append(currencyFor(product));
+              xml.append("'>");
+              xml.append(product.getPrice());
+              xml.append("</price>");
+              xml.append(product.getName());
+              xml.append("</product>");
             }
-            xml.append(">");
-            xml.append("<price");
-            xml.append(" currency='");
-            xml.append(currencyFor(product));
-            xml.append("'>");
-            xml.append(product.getPrice());
-            xml.append("</price>");
-            xml.append(product.getName());
-            xml.append("</product>");
-          }
-        xml.append("</order>");
-      }
-      xml.append("</orders>");
-      return xml.toString();
+          xml.append("</order>");
+        }
+        xml.append("</orders>");
     }
 
 
